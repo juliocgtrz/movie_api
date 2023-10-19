8d1b68e125a204b0285946c0c87dbfa39aa80caa
Julio G <juliocgtrz@gmail.com>
2023-10-18 23:43:30 -0600

    Update index.js
    updated movie list information

--- index.js
+++ index.js
@@ -12,44 +12,49 @@ const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), {f
 let movies = [
     {
         title: 'The Sandlot',
-        director: 'David Mickey Evans'
+        description: 'In the summer of 1962, a new kid in town is taken under the wing of a young baseball prodigy and his rowdy team, resulting in many adventures.',
+        genre: {
+            name: 'Comedy',
+            description: 'A drama of light and amusing character and typically with a happy ending.'
+        },
+        director: {
+        name: 'David Mickey Evans',
+        bio: 'David Mickey Evans was born on October 20, 1962 in Wilkes Barre, Pennsylvania, USA. He is a director and writer, known for The Sandlot (1993), Untitled Sandlot Prequel and Radio Flyer (1992).',
+        birth: '1962'
+        },
+        imageURL: 'https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/6C12E7878CF4642CE13C3DF5E64D6584802F1363ABE86BCBEFCA297B239B2E24/scale?width=1200&aspectRatio=1.78&format=jpeg',
+        featured: false
     },
     {
         title: 'A League of Their Own',
-        director: 'Penny Marshall'
+        description: 'Two sisters join the first female professional baseball league and struggle to help it succeed amid their own growing rivalry.',
+        genre: {
+            name: 'Comedy',
+            description: 'A drama of light and amusing character and typically with a happy ending.'
+        },
+        director: {
+            name: 'Penny Marshall',
+            bio: 'Penny Marshall was born Carole Penny Marshall on October 15, 1943 in Manhattan. She was the daughter of Marjorie (Ward), a tap dance teacher, and Anthony Marshall, an industrial film director. She was the younger sister of filmmakers Garry Marshall and Ronny Hallin.',
+            birth: '1943'
+        },
+        imageURL: 'https://m.media-amazon.com/images/M/MV5BNjQ1NTQyYjktZDc5My00NDA1LWI1NmItY2ViNjQ0NDk4NmRjXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_.jpg',
+        featured: false
     },
     {
         title: 'Bull Durham',
-        director: 'Ron Shelton'
+        description: 'A fan who has an affair with one minor-league baseball player each season meets an up-and-coming pitcher and the experienced catcher assigned to him.',
+        genre: {
+            name: 'Comedy',
+            description: 'A drama of light and amusing character and typically with a happy ending.'
+        },
+        director: {
+            name: 'Ron Shelton',
+            bio: 'Ron Shelton was born on 15 September 1945 in Whittier, California, USA. He is a writer and director, known for Bull Durham (1988), Hollywood Homicide (2003) and White Men Cant Jump (1992). He has been married to Lolita Davidovich since 1997. They have one child.',
+            birth: '1945'
+        },
+        imageURL: 'https://m.media-amazon.com/images/M/MV5BMzMxMDEzMWUtZDk3NS00MWRiLWJjOGMtN2Q0ZjVhZjU3ODhkXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_FMjpg_UX1000_.jpg',
+        featured: false
     },
-    {
-        title: 'Major League',
-        director: 'David S. Ward'
-    },
-    {
-        title: 'The Bad News Bears',
-        director: 'Michael Ritchie'
-    },
-    {
-        title: 'Rookie of the Year',
-        director: 'Daniel Stern'
-    },
-    {
-        title: 'Angels in the Outfield',
-        director: 'William Dear'
-    },
-    {
-        title: 'Field of Dreams',
-        director: 'Phil Alden Robinson'
-    },
-    {
-        title: 'Fever Pitch',
-        director: 'Bobby Farrelly'
-    },
-    {
-        title: '42',
-        director: 'Brian Helgeland'
-    },   
 ];
 
 app.use(morgan('combined', {stream: accessLogStream}));
