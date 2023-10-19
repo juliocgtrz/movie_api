90ece85e448b7a151be2479a4abb2eb553ca7fb2
Julio G <juliocgtrz@gmail.com>
2023-10-18 23:16:35 -0600

    Update index.js
    brought back list of movies and changed name to movies

--- index.js
+++ index.js
@@ -9,54 +9,54 @@ const app = express();
 
 const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), {flags:'a'});
 
-// let topTenMovies = [
-//     {
-//         title: 'The Sandlot',
-//         director: 'David Mickey Evans'
-//     },
-//     {
-//         title: 'A League of Their Own',
-//         director: 'Penny Marshall'
-//     },
-//     {
-//         title: 'Bull Durham',
-//         director: 'Ron Shelton'
-//     },
-//     {
-//         title: 'Major League',
-//         director: 'David S. Ward'
-//     },
-//     {
-//         title: 'The Bad News Bears',
-//         director: 'Michael Ritchie'
-//     },
-//     {
-//         title: 'Rookie of the Year',
-//         director: 'Daniel Stern'
-//     },
-//     {
-//         title: 'Angels in the Outfield',
-//         director: 'William Dear'
-//     },
-//     {
-//         title: 'Field of Dreams',
-//         director: 'Phil Alden Robinson'
-//     },
-//     {
-//         title: 'Fever Pitch',
-//         director: 'Bobby Farrelly'
-//     },
-//     {
-//         title: '42',
-//         director: 'Brian Helgeland'
-//     },   
-// ];
+let movies = [
+    {
+        title: 'The Sandlot',
+        director: 'David Mickey Evans'
+    },
+    {
+        title: 'A League of Their Own',
+        director: 'Penny Marshall'
+    },
+    {
+        title: 'Bull Durham',
+        director: 'Ron Shelton'
+    },
+    {
+        title: 'Major League',
+        director: 'David S. Ward'
+    },
+    {
+        title: 'The Bad News Bears',
+        director: 'Michael Ritchie'
+    },
+    {
+        title: 'Rookie of the Year',
+        director: 'Daniel Stern'
+    },
+    {
+        title: 'Angels in the Outfield',
+        director: 'William Dear'
+    },
+    {
+        title: 'Field of Dreams',
+        director: 'Phil Alden Robinson'
+    },
+    {
+        title: 'Fever Pitch',
+        director: 'Bobby Farrelly'
+    },
+    {
+        title: '42',
+        director: 'Brian Helgeland'
+    },   
+];
 
 app.use(morgan('combined', {stream: accessLogStream}));
 app.use('/documentation.html', express.static('public'));
 
 app.get('/movies', (req, res) => {
-    res.json(topTenMovies);
+    res.json(movies);
 });
 
 app.get('/', (req, res) => {
