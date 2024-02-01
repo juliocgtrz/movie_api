63939b1698713bb99817f0fe7f47d6060da2aa73
Julio G <juliocgtrz@gmail.com>
2024-01-31 23:39:07 -0700

    Update index.js
    updated function to delete to remove a user by username

--- index.js
+++ index.js
@@ -178,7 +178,7 @@ app.delete('/users/:Username/movies/:MovieID', async (req, res) => {
 
 //Delete a user by username
 app.delete('/users/:Username', async (req, res) => {
-    await Users.findOneAndRemove({ Username: req.params.Username })
+    await Users.findOneAndDelete({ Username: req.params.Username })
         .then((user) => {
             if (!user) {
                 res.status(400).send(req.params.Username + ' was not found');
