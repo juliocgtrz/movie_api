c3b8c1a04cc39c6814d27c5865aebf83cf67fd18
Julio G <juliocgtrz@gmail.com>
2024-02-20 21:38:50 -0700

    Update index.js
    added code to only give specific origins access o the API

--- index.js
+++ index.js
@@ -15,7 +15,18 @@ mongoose.connect('mongodb://localhost:27017/myflixDB', { useNewUrlParser: true, 
 const app = express();
 const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), {flags:'a'});
 const cors = require('cors'); //Code to use CORS within myFlix
-app.use(cors());
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
+
+app.use(cors({
+    origin: (origin, callback) => {
+        if(!origin) return callback(null, true);
+        if(allowedOrigins.indexOf(origin) === -1){ //If a specific origin isn't found on the list of allowed origins
+            let message = 'The CORS policy for this application doesn\'t allow access from origin ' + origin;
+            return callback(new Error(message ), false);
+        }
+        return callback(null, true);
+    }
+}));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
