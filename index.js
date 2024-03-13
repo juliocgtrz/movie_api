74c5e4f9fa322afca33f45c88b987b6d8f273e75
Julio G <juliocgtrz@gmail.com>
2024-03-12 23:19:51 -0600

    Update index.js
    added authentication back in

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ app.use((err, req, res, next) => {
 });
 
 //Get all movies
-app.get('/movies', async (req, res) => {
+app.get('/movies', passport.authenticate('jwt', { session: false }), async (req, res) => {
     await Movies.find()
         .then((movies) => {
             res.status(201).json(movies);
