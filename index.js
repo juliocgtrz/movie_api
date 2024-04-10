79cb273646c388f184e24d5abd59ec6a58e19e84
Julio G <juliocgtrz@gmail.com>
2024-04-09 22:41:42 -0600

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ mongoose.connect( process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifie
 const app = express();
 const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), {flags:'a'});
 const cors = require('cors'); //Code to use CORS within myFlix
-let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234', 'http://localhost:1234/', 'https://myflix-baseball.netlify.app/'];
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234', 'http://localhost:1234/', 'https://myflix-baseball.netlify.app'];
 
 app.use(cors({
     origin: (origin, callback) => {
